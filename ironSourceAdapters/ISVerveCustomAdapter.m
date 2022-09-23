36e8b742be17e054ad41c2fa42571916f48a2ce7
Can SOYKARAFAKILI <can.soykarafakili@gmail.com>
2022-09-23 17:39:26 +0200

    v: 2.16.0.0

--- ironSourceAdapters/ISVerveCustomAdapter.m
+++ ironSourceAdapters/ISVerveCustomAdapter.m
@@ -41,11 +41,11 @@
 }
 
 - (NSString *)networkSDKVersion {
-    return @"2.15.0";
+    return @"2.16.0";
 }
 
 - (NSString *)adapterVersion {
-    return @"2.15.0.0";
+    return @"2.16.0.0";
 }
 
 @end
