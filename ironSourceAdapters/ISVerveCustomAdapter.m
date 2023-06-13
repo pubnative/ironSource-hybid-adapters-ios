90ee3577da0fbf96c54a5fff88e42b556cfe8a4e
José Jacobo Contreras Trejo <josejacoboct@gmail.com>
2023-06-13 16:54:06 +0200

    v: 2.19.0-beta2.0

--- ironSourceAdapters/ISVerveCustomAdapter.m
+++ ironSourceAdapters/ISVerveCustomAdapter.m
@@ -41,11 +41,11 @@
 }
 
 - (NSString *)networkSDKVersion {
-    return @"2.19.0-beta";
+    return @"2.19.0-beta2";
 }
 
 - (NSString *)adapterVersion {
-    return @"2.19.0-beta.0";
+    return @"2.19.0-beta2.0";
 }
 
 @end
